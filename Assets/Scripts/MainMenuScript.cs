856406af552e0127606c35ff26bb5889db6c8f65
Kyle Greene <kmgreene@gmail.com>
2024-02-26 11:59:43 -0800

    Fixed exit button on main menu to work

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -13,4 +13,13 @@ public class MainMenuScript : MonoBehaviour
     {
         SceneManager.LoadScene("Singleplayer");
     }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
